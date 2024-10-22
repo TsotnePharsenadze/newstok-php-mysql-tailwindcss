dc56fb30b172a278f474b1683b406a1d37790d15
Tsotne Pharsenadze <wallemechson@gmail.com>
2024-10-23 00:39:43 +0400

    implemented even more filters

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 include "./db/db.php";
 include "./helpers/icons.php";
-$result2 = $conn->query("SELECT DISTINCT category from furniture");
-$category = $_GET["category"];
+$pageSize = 12;
+$dataCount = null;
+if (isset($_GET["page"]) && is_numeric($_GET["page"]) && $_GET["page"] > 0) {
+    $page = (int)$_GET["page"];
+} else {
+    $page = 1;
+}
+$resultCategories = $conn->query("SELECT DISTINCT category from furniture");
+$resultColors = $conn->query("SELECT DISTINCT color from furniture");
+$resultBrands = $conn->query("SELECT DISTINCT brand from furniture");
+$resultPrices = $conn->query("SELECT MIN(price) as min_price, MAX(price) as max_price from furniture");
+$resultPricesFetch = $resultPrices->fetch_assoc();
+$category = $_GET["category"] ?? null;
+$colors = $_GET["colors"] ?? null;
+$brand = $_GET["brand"] ?? null;
+$price = $_GET["price"] ?? null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +31,7 @@ $category = $_GET["category"];
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
-<body class="p-1 md:p-6 max-w-[1000px] mx-auto w-full">
+<body class="p-1 md:p-6 max-w-[1000px] mx-auto w-full shadow-lg">
     <header class="flex flex-col">
         <nav>
             <ul class="flex justify-between">
@@ -45,8 +59,8 @@ $category = $_GET["category"];
         </nav>
         <div class="bg-gray-100 sm:px-6 flex justify-center mt-4">
             <?php
-            if ($result2->num_rows > 0) {
-                while ($row = $result2->fetch_assoc()) { ?>
+            if ($resultCategories->num_rows > 0) {
+                while ($row = $resultCategories->fetch_assoc()) { ?>
                     <div class="flex flex-col justify-center items-center p-2 opacity-40 hover:opacity-80 hover:bg-white w-[150px] cursor-pointer <?php if ($category == null) {
                         $category = strtolower($row["category"]);
                         echo "opacity-80 bg-white";
@@ -61,27 +75,27 @@ $category = $_GET["category"];
                     </div>
                 <?php }
             } ?>
+            <?php
+            $stmt = $conn->prepare("SELECT COUNT(*) as data_count FROM furniture WHERE category = ?");
+            $stmt->bind_param("s", $category);
+            $stmt->execute();
+            $resultCount = $stmt->get_result();
+            $dataCount = $resultCount->fetch_assoc()["data_count"];
+            ?>
         </div>
         <div class="mt-6 mb-6 border-b-[.45rem] border-gray-100 p-6">
             <div class="flex justify-center items-center">
-                <div class="flex flex-1 space-x-4 items-center px-2">
-                    <div>
-                        <input type="checkbox" name="type" id="chairs" value="chairs" />
-                        <label for="chairs" class="text-gray-700">Chairs</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" name="type" id="ottomans" value="ottomans" />
-                        <label for="ottomans" class="text-gray-700">Ottomans</label>
-                    </div>
-                </div>
-
-                <div class="flex flex-1 border-l-2 border-r-2 px-2 border-black/50">
+                <div class="flex flex-1 border-r-2 px-2 border-black/50">
                     <select name="colors"
                         class="w-full text-gray-700 p-2 rounded focus:outline-none focus:ring-2 focus:ring-gray-400">
                         <option value="anycolors">Any Colors</option>
-                        <option value="red">Red</option>
-                        <option value="blue">Blue</option>
-                        <option value="green">Green</option>
+                        <?php
+                        if ($resultColors->num_rows > 0) {
+                            while ($row = $resultColors->fetch_assoc()) { ?>
+                                <option value="<?php echo $row["color"]; ?>"><?php echo ucfirst($row["color"]); ?></option>
+                            <?php }
+                        }
+                        ?>
                     </select>
                 </div>
 
@@ -89,17 +103,23 @@ $category = $_GET["category"];
                     <select name="designer"
                         class="w-full text-gray-700 p-2 rounded focus:outline-none focus:ring-2 focus:ring-gray-400">
                         <option value="anydesigner">Any Designer</option>
-                        <option value="red">Red</option>
-                        <option value="blue">Blue</option>
-                        <option value="green">Green</option>
+                        <?php
+                        if ($resultBrands->num_rows > 0) {
+                            while ($row = $resultBrands->fetch_assoc()) { ?>
+                                <option value="<?php echo $row["brand"]; ?>"><?php echo ucfirst($row["brand"]); ?></option>
+                            <?php }
+                        }
+                        ?>
                     </select>
                 </div>
 
                 <div class="flex flex-1 gap-2 px-2 items-center">
                     <span class="text-gray-700">Price</span>
-                    <input type="range"
+                    <input type="range" min="<?php echo $resultPricesFetch["min_price"]; ?>"
+                        max="<?php echo $resultPricesFetch["max_price"]; ?>"
+                        value="<?php echo $resultPricesFetch["max_price"]; ?>" oninput="updateValue(this.value)"
                         class="h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400" />
-                    <span class="text-gray-700">$9500</span>
+                    <span id="priceRange" class="text-gray-700">$<?php echo $resultPricesFetch["max_price"]; ?></span>
                 </div>
             </div>
         </div>
@@ -119,15 +139,16 @@ $category = $_GET["category"];
         </header>
         <div class="grid mt-4 gap-4 grid-cols-1 mb-6 border-gray-100 sm:grid-cols-2 md:grid-cols-4 p-4">
             <?php
-            $stmt = $conn->prepare("SELECT * FROM furniture WHERE category = ?");
-            $stmt->bind_param("s", $category);
+            $offset = ((int) $page - 1) * $pageSize;
+            $stmt = $conn->prepare("SELECT * FROM furniture WHERE category = ? LIMIT ? OFFSET ?");
+            $stmt->bind_param("sii", $category, $pageSize, $offset);
             $stmt->execute();
             $result = $stmt->get_result();
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) { ?>
                     <div
                         class="flex flex-col items-center hover:scale-105 shadow-sm cursor-pointer hover:shadow-md transition-all pb-4">
-                        <div class="w-full h-48 bg-gray-400"></div>
+                        <img src="./storage/public/chair.png" class="w-full h-48" />
                         <p class="mt-2"><?php echo $row['name'] ?></p>
                         <p class="text-lg font-bold">$<?php echo $row['price'] ?></p>
                     </div>
@@ -138,12 +159,29 @@ $category = $_GET["category"];
         </div>
 
         <footer class=" border-t-[.45rem] p-4 flex justify-between pb-[4rem]">
-            <h2 class="cursor-pointer hover:underline">Show more chairs</h2>
-            <div>
+
+            <h2 class="w-full max-w-[200px] <?php if ($page <= 1) {
+                echo "opacity-50 cursor-default";
+            } else {
+                echo "cursor-pointer hover:underline ";
+            } ?>" <?php if ($page > 1) {
+                 echo 'onClick="showMore(1)"';
+             } ?>>Go back</h2>
+
+
+            <div class="mx-auto">
                 <span class="text-lg text-gray-400">&larr;</span>
-                <span class="text-lg font-bold">3</span>
+                <span class="text-lg font-bold"><?php echo $page; ?></span>
                 <span class="text-lg text-gray-400">&rarr;</span>
             </div>
+
+            <h2 class="text-right w-full max-w-[200px] <?php if ($page >= ceil($dataCount / $pageSize)) {
+                echo "opacity-50 cursor-default";
+            } else {
+                echo "cursor-pointer hover:underline  ";
+            } ?>" <?php if ($page < ceil($dataCount / $pageSize)) {
+                 echo 'onClick="showMore()"';
+             } ?>>Show more</h2>
         </footer>
     </main>
     <footer class="bg-gray-100 pt-14 pb-8 flex space-y-2 flex-col justify-center items-center">
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
 function changeCategory(category) {
-    window.location.href = `?category=${category}`;
+  window.location.href = `?category=${category}`;
+}
+
+function updateValue(value) {
+  document.querySelector("#priceRange").textContent = "$" + value;
+}
+
+function showMore(goBack) {
+  if (!goBack) {
+    const url = new URL(window.location.href);
+
+    let currentPage = parseInt(url.searchParams.get("page")) || 1;
+
+    currentPage += 1;
+
+    url.searchParams.set("page", currentPage);
+
+    window.location.href = url.toString();
+  } else {
+    const url = new URL(window.location.href);
+
+    let currentPage = parseInt(url.searchParams.get("page")) || 1;
+
+    currentPage -= 1;
+
+    url.searchParams.set("page", currentPage);
+
+    window.location.href = url.toString();
+  }
 }
