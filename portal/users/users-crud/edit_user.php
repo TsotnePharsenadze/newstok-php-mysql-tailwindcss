34a05378d8afac40c884f1d9dd555d740cd7ab3f
Tsotne Pharsenadze <wallemechson@gmail.com>
2024-12-19 02:10:16 +0400

    fixed edit-user page

--- portal/users/users-crud/edit_user.php
+++ portal/users/users-crud/edit_user.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Newstok - Edit User</title>
+    <title>usertok - Edit User</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         function goBack() {
@@ -73,44 +73,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <div class="mb-4">
                     <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                     <input type="text" name="email" id="email" class="p-2 mr-2 border rounded-md w-full" value="<?php
-                    echo $news["email"] ?? "";
+                    echo $user["email"] ?? "";
                     ?>" required>
                 </div>
                 <div class="mb-4">
                     <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
                     <input type="text" name="username" id="username" class="p-2 mr-2 border rounded-md w-full" value="<?php
-                    echo $news["username"] ?? "";
+                    echo $user["username"] ?? "";
                     ?>" required>
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                     <input type="password" name="password" id="password" class="p-2 mr-2 border rounded-md w-full"
                         value="<?php
-                        echo $news["password"] ?? "";
+                        echo $user["password"] ?? "";
                         ?>" required>
                 </div>
                 <div class="mb-4">
                     <label for="display_name" class="block text-sm font-medium text-gray-700">Display Name</label>
                     <input type="text" name="display_name" id="display_name" class="p-2 mr-2 border rounded-md w-full"
                         value="<?php
-                        echo $news["display_name"] ?? "";
+                        echo $user["display_name"] ?? "";
                         ?>" required>
                 </div>
                 <div class="mb-4">
                     <label for="sts" class="block text-sm font-medium text-gray-700">Status</label>
                     <div class="flex">
                         <div class="flex">
-                            <input type="radio" name="sts" id="sts1" class="p-2 mr-2 border rounded-md" value="1" value="<?php
-                            echo $news["sts"] == 1 ? "checked" : "";
-                            ?>">
+                            <input type="radio" name="sts" id="sts1" class="p-2 mr-2 border rounded-md" value="1" <?php
+                            echo $user["sts"] == 1 ? "checked" : "";
+                            ?>>
                             <label for="sts1"><span
                                     class="bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded uppercase">Pending</span></label>
                         </div>
                         <div class="flex">
-                            <input type="radio" name="sts" id="sts2" class="p-2 mr-2 border rounded-md" value="2" value="<?php
-                            echo $news["sts"] == 2 ? "checked" : "";
-                            ?>">
-                            <label for="sts2"><span
+                            <input type="radio" name="sts" id="sts2" class="p-2 mr-2 border rounded-md" value="2" <?php
+                            echo $user["sts"] == 2 ? "checked" : "";
+                            ?>> <label for="sts2"><span
                                     class="bg-green-100 text-green-800 font-medium me-2 px-2.5 py-0.5 rounded uppercase">Approved</span></label>
                         </div>
                     </div>
@@ -120,17 +119,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <div class="flex">
                         <div class="flex">
                             <input type="radio" name="type" id="type1" class="p-2 mr-2 border rounded-md" value="editor"
-                                value="<?php
-                                echo $news["type"] == "editor" ? "checked" : "";
-                                ?>">
+                                <?php
+                                echo $user["type"] == "editor" ? "checked" : "";
+                                ?>>
                             <label for="type1"><span
                                     class="bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded uppercase">Editor</span></label>
                         </div>
                         <div class="flex">
                             <input type="radio" name="type" id="type2" class="p-2 mr-2 border rounded-md" value="admin"
-                                value="<?php
-                                echo $news["type"] == "admin" ? "checked" : "";
-                                ?>">
+                                <?php
+                                echo $user["type"] == "admin" ? "checked" : "";
+                                ?>>
                             <label for="type2"><span
                                     class="bg-green-100 text-green-800 font-medium me-2 px-2.5 py-0.5 rounded uppercase">Admin</span></label>
                         </div>
